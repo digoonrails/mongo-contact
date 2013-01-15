dd220992ac53af7dda8a0843075c305be7da68c5
Rodrigo Pestana <rodrigo.pest@gmail.com>
2013-01-15 09:02:17 -0200

    Initial project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoContact::Application.config.secret_token = '6065c8a87bacaeb1c479b9e8d0556c82ac875bfcfaa58efd67b9d081dd99ae299820435e026b4708b8e0b0a012718a642f8d850083e2b5898bc44f45c0bece6b'
